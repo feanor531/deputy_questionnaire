93818a224533e469244ac17521a1d5c8f7bbb59f
feanor531 <40800105+feanor531@users.noreply.github.com>
2022-01-07 12:34:12 +0200

    Add files via upload

---
+++ Emploee.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_5
+{
+   
+    internal class Emploee : INotifyPropertyChanged, IEditableObject
+    {
+        private ItemData _copyData;
+        private ItemData _currentData;
+
+
+        //private string first_name;
+        //private string second_name;
+        //private string surname;
+        //private int birth_year;
+        //private string education; // образование
+        //private string languages;
+        //private string level_language;
+        //private string computer_skills;
+        //private int experients; //стаж работы
+        //private string recomendation;
+
+        public Emploee() { }
+        public Emploee(string _first_name, string _second_name,
+                string _surname, int _birth_year,
+                string _education, string _languages, string _level_language,
+                string _computer_skills, int _experients, string _recomendation)
+        {
+            FirstName = _first_name;
+            SecondName = _second_name;
+            Surnaname = _surname;
+            BirthYear = _birth_year;
+            Education = _education;
+            Languages = _languages;
+            LevelLanguage = _level_language;
+            ComputerSkills = _computer_skills;
+            Experients = _experients;
+            Recomendation = _recomendation;
+        }
+
+        public string FirstName
+        {
+            get { return _currentData.FirstName; }
+            set
+            {
+                _currentData.FirstName = value;
+                NotifyPropertyChanged("FirstName");
+            }
+        }
+        public string SecondName
+        {
+            get { return _currentData.SecondName; }
+            set
+            {
+                _currentData.SecondName = value;
+                NotifyPropertyChanged("SecondName");
+            }
+        }
+        public string Surnaname
+        {
+            get { return _currentData.Surnaname; }
+            set
+            {
+                _currentData.Surnaname = value;
+                NotifyPropertyChanged("Surnaname");
+            }
+        }
+        public int BirthYear
+        {
+            get { return _currentData.BirthYear; }
+            set
+            {
+                _currentData.BirthYear = value;
+                NotifyPropertyChanged("BirthYear");
+            }
+        }
+        public string Education
+        {
+            get { return _currentData.Education; }
+            set
+            {
+                _currentData.Education = value;
+                NotifyPropertyChanged("Education");
+            }
+        }
+        public string Languages
+        {
+            get { return _currentData.Languages; }
+            set
+            {
+                _currentData.Languages = value;
+                NotifyPropertyChanged("Languages");
+            }
+        }
+        public string LevelLanguage
+        {
+            get { return _currentData.LevelLanguage; }
+            set
+            {
+                _currentData.LevelLanguage = value;
+                NotifyPropertyChanged("LevelLanguage");
+            }
+        }
+        public string ComputerSkills
+        {
+            get { return _currentData.ComputerSkills; }
+            set
+            {
+                _currentData.ComputerSkills = value;
+                NotifyPropertyChanged("ComputerSkills");
+            }
+        }
+        public int Experients
+        {
+            get { return _currentData.Experients; }
+            set
+            {
+                _currentData.Experients = value;
+                NotifyPropertyChanged("Experients");
+            }
+        }
+        public string Recomendation
+        {
+            get { return _currentData.Recomendation; }
+            set
+            {
+                _currentData.Recomendation = value;
+                NotifyPropertyChanged("Recomendation");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string arg)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(arg));
+        }
+
+        public override string ToString() => $"{Surnaname} {FirstName} {SecondName}";
+        private struct ItemData
+        {
+            internal string FirstName;
+            internal string SecondName;
+            internal string Surnaname;
+            internal int BirthYear;
+            internal string Education;
+            internal string Languages;
+            internal string LevelLanguage;
+            internal string ComputerSkills;
+            internal int Experients;
+            internal string Recomendation;
+        }
+
+        public void BeginEdit()
+        {
+            _copyData = _currentData;
+        }
+
+        public void CancelEdit()
+        {
+            _currentData = _copyData;
+            NotifyPropertyChanged("");
+        }
+
+        public void EndEdit()
+        {
+            _copyData = new ItemData();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DZ_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+    /// Для участия в конкурсе на замещение вакантной
+    //    должности сотрудника предприятия желающие
+    //подают информацию:
+    //■Ф.И.О.;
+    //■год рождения;
+    //■образование(среднее, среднее специальное,
+    //высшее);
+    //■знание иностранных языков(английский,
+    //немецкий, французский; владею свободно, читаю и
+    //перевожу со словарем);
+    //■владение компьютером;
+    //■стаж работы;
+    //■наличие рекомендаций;
+    //    Создайте приложение, которое должно
+    //    позволять выводить информацию о тех, кто подал
+    //    заявку на участие в конкурсе с учетом вывода всех
+    //или выбора по каким-то характеристикам(выберите
+    //самостоятельно).
+    //При разработке приложения необходимо
+    //использовать ресурсы, стили.
+    //Задание 2(дополнительное) При создании приложения
+    //создайте несколько окон для вывода необходимой
+    //информации, в зависимости от выбора просмотра
+
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            var editableCollectionView = itemsControl.Items as IEditableCollectionView;
+            if (!editableCollectionView.CanAddNew)
+            {
+                MessageBox.Show("Вы не можете добавлять элементы в список.");
+                return;
+            }
+            var win = new newWorksheet { DataContext = editableCollectionView.AddNew() };
+            if ((bool)win.ShowDialog())
+            {
+                editableCollectionView.CommitNew();
+                itemsControl.Items.Refresh();
+            }
+            else
+            {
+                editableCollectionView.CancelNew();
+            }
+        }
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (itemsControl.SelectedItem == null)
+            {
+                MessageBox.Show("Тема не выбрана");
+                return;
+            }
+            var editableCollectionView = itemsControl.Items as IEditableCollectionView;
+            var win = new newWorksheet();
+            editableCollectionView.EditItem(itemsControl.SelectedItem);
+            win.DataContext = itemsControl.SelectedItem;
+
+            if ((bool)win.ShowDialog())
+            {
+                editableCollectionView.CommitEdit();
+                itemsControl.Items.Refresh();
+            }
+            else
+            {
+                editableCollectionView.CancelEdit();
+            }
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            var item = itemsControl.SelectedItem as Emploee;
+            if (item == null)
+            {
+                MessageBox.Show("Тема не выбрана");
+                return;
+            }
+            var editableCollectionView = itemsControl.Items as IEditableCollectionView;
+            if (!editableCollectionView.CanRemove)
+            {
+                MessageBox.Show("Вы не можете удалить элемент из списка");
+                return;
+            }
+            if (MessageBox.Show("Ты уверен что хочешь удалить " + item.Surnaname +" "+item.FirstName, 
+                "Удаление анкеты", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                editableCollectionView.Remove(itemsControl.SelectedItem);
+            }
+        }
+    }
+}
